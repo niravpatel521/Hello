f261dd2d5950336f0f65f276321f9d2188161577
Nirav Patel <nirav.patel@botreetechnologies.com>
2015-10-05 21:58:46 +0530

    #2 -> new_branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  
+  root 'homes#home'
+
+  #root 'application#hello'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
